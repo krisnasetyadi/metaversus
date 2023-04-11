414256ad17099045261de35359b5878ace898896
krisnasetyadi <krisnadwisetyadi@gmail.com>
2023-04-11 11:54:03 +0700

    add footer

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -41,7 +41,7 @@ const Feedback = () => (
         />
         <motion.div
           variants={zoomIn(0.4, 1)}
-          className="lg:block hidden absolute -left[10%] top-[3%]"
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
         >
           <img
             src="/stamp.png"
